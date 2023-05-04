d4632959e0c43f190c55c4f04dc2a5dcb5d7c228
Ai-Shaf <herderess@gmail.com>
2023-05-04 14:21:12 -0700

    adding ModelAdmin for Assignment 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts')
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Category)
