5e270631dd89424b06968a1be3fd04c787333f6d
Ai-Shaf <herderess@gmail.com>
2023-05-09 17:54:16 -0700

    cleaning out stub view

--- blogging/views.py
+++ blogging/views.py
@@ -6,18 +6,6 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.detail import DetailView
 
 
-# Create your views here.
-# def stub_view(request, *args, **kwargs):
-#     body = "Stub View\n\n"
-#     if args:
-#         body += "Args:\n"
-#         body += "\n".join(["\t%s" % a for a in args])
-#     if kwargs:
-#         body += "Kwargs:\n"
-#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-#     return HttpResponse(body, content_type="text/plain")
-
-
 class BlogListView(ListView):
     queryset = Post.objects.order_by('-published_date').exclude(published_date__exact=None)
     template_name = 'blogging/list.html'
